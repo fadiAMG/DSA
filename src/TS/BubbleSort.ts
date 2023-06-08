fe6672521b3471638d779806fc263cf4f6ecd7b2
fadi amged <fadiamged780@gmail.com>
2023-06-08 20:13:47 +0300

    bubbleSort

---
+++ src/TS/BubbleSort.ts
@@ -0,0 +1,15 @@
+function bubbleSort(array: number[]): number[] {
+    for (let i: number = 0; i < array.length; ++i) {
+        for (let j: number = 0; j < array.length - i - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+let sampleArray: number[] = [234, 43, 55, 63, 5, 6, 235, 547, 1555, 20, 37]
+console.log("BubbleSorted", bubbleSort(sampleArray))
